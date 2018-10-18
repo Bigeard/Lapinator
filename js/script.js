26c061cd2f7f5f2a9870fc7cba6c0c29d0bb791c
Bigeard <robin.bigeard@gmail.com>
2018-10-18 12:32:58 +0200

    Je pense que je vais m'arrete la

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var x = document.getElementById("menu");
+var i = document.getElementById("icon");
+
+
+window.onresize = function (event) {
+    detectSize();
+};
+
+window.onload = function (event) {
+    detectSize();
+};
+
+function detectSize() {
+    if (window.innerWidth <= 580) {
+        mobile = true;
+        x.style.display = "none";
+        i.style.display = "block";
+    } else {
+        mobile = false;
+        i.style.display = "none";
+        x.style.display = "flex";
+    }
+}
+
+function clickMenu() {
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        i.style.transform = "rotate(180deg)"
+    } else {
+        x.style.display = "block";
+        i.style.transform = "rotate(90deg)"
+    }
+}
