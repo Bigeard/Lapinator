5fcd146171dc0bdaa8f9bc2f4e0d8b9360cc438f
Bigeard <robin.bigeard@gmail.com>
2018-10-19 11:28:29 +0200

    Hi world

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-var x = document.getElementById("menu");
+var x = document.getElementById("items-menu");
 var i = document.getElementById("icon");
-
+var n = document.getElementById("nav");
 
 window.onresize = function () {
     detectSize();
@@ -11,11 +11,14 @@ window.onload = function () {
 };
 
 function detectSize() {
+
     if (window.innerWidth <= 580) {
         mobile = true;
         x.style.display = "none";
         i.style.display = "block";
         i.style.transform = "rotate(180deg)"
+        n.style.backgroundColor = "transparent";
+        x.style.backgroundColor = "transparent";
     } else {
         mobile = false;
         i.style.display = "none";
@@ -25,10 +28,43 @@ function detectSize() {
 
 function clickMenu() {
     if (x.style.display === "block") {
+        n.style.backgroundColor = "transparent";
+        x.style.backgroundColor = "transparent";
         x.style.display = "none";
-        i.style.transform = "rotate(180deg)"
+        i.style.transform = "rotate(180deg)";
     } else {
+        n.style.backgroundColor = "white";
+        x.style.backgroundColor = "white";
+
         x.style.display = "block";
-        i.style.transform = "rotate(90deg)"
+        x.style.opacity = "1";
+        i.style.transform = "rotate(90deg)";
+    }
+}
+
+// var form = document.getElementById('form');
+
+// // Add a listener to the submit event
+// form.addEventListener('submit', function (e) {
+//     var errors = [];
+
+//     if(document.getElementById("text").value == "hello world") {
+//         window.location = "blog.html";
+//     } else {
+//         document.getElementById("error").innerHTML = "Fail authentification..."
+//     }
+    
+
+//     if(errors.length) {
+//          e.preventDefault(); // The browser will not make the HTTP POST request
+//          return;
+//     }
+// });
+
+function clickSubmit() {
+    if(document.getElementById("text").value == "hello world") {
+        window.location = "blog.html";
+    } else {
+        document.getElementById("error").innerHTML = "Fail authentification..."
     }
 }
